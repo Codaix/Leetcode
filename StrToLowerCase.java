a26989625bc41a716d1f8492d170b42306584291
Codaix <160418176+Codaix@users.noreply.github.com>
2025-07-24 15:03:20 +0530

    StrToLowerCase

---
+++ StrToLowerCase.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // Convert string to lowercase and remove non-alphanumeric characters
+        StringBuilder cleaned = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+
+        // Two-pointer approach to check for palindrome
+        int left = 0;
+        int right = cleaned.length() - 1;
+
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
